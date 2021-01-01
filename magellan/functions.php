7122d9d43901c8650113d7c7bc8bd3eaa32613ee
Toshiaki <conamn1185@gmail.com>
2021-01-01 20:32:43 +0900

    change db settings for deploy

--- magellan/functions.php
+++ magellan/functions.php
@@ -3,8 +3,16 @@
         // 主要な変数群
         //    PDOクラスのインスタンスを生成する(エラーモードも同時にセット)
 
-        $pdo = new PDO('mysql:host=127.0.0.1;dbname=magellan;charset=utf8', 'root', 'honoka1027');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+        $db['dbname'] = ltrim($db['path'], '/');
+        $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+        $user = $db['user'];
+        $password = $db['pass'];
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
+        );
+        $pdo = new PDO($dsn, $user, $password, $options);
 
 
         // 複数回使用する各種関数をまとめた
