6941bf9479f0564eae0e3ac7923ae36200fd0343
Toshiaki <conamn1185@gmail.com>
2021-01-01 23:10:52 +0900

    index.php set to direct Login.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+	$uri = 'https://';
+} else {
+	$uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+header('Location: ' . $uri . '/Login.php');
+exit;
 ?>
 Something is wrong with the XAMPP installation :-(
